e5bc481c746c47ec3532f72832a308ab348e391c
olederma <oliverhubertus@gmx.de>
2022-12-04 21:34:40 +0100

    solution 3.1

--- 4.1/main.py
+++ 4.1/main.py
@@ -34,14 +34,41 @@ def get_value_of_backpack(item_list):
     return calculate_poitns(doubble_letter)
 
 
+def find_same_letter(list_of_group):
+    for i in range(len(list_of_group[0])-1):
+        for j in range(len(list_of_group[1])-1):
+            for k in range(len(list_of_group[2])-1):
+                if list_of_group[0][i] == list_of_group[1][j] == list_of_group[2][k]:
+                    return calculate_poitns(list_of_group[2][k])
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    test = 0
     points = 0
+    iterator = 0
+    group_list = []
     f = open("data", "r")
+    testi = 0
     for x in f:
-        points = points + get_value_of_backpack(x)
-        print("actual points: " + str(points))
+        testi = testi + 1
+        if iterator > 1:
+            iterator = 0
+            group_list.append(x)
+            points = points + find_same_letter(group_list)
+            group_list = []
+            print("yes")
+            print(x)
+        else:
+            iterator = iterator + 1
+            group_list.append(x)
+            print("no")
+            print(x)
+        if testi > 297:
+            print("jhdhjd")
+
+
     print(points)
 
 
